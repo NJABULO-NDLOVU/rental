db70c34511837f63bed8d26e2f8a28f68e869bbd
mmitjam <mp.mmotja@gmail.com>
2022-02-12 16:52:16 +0200

    Added shopping list and item table

--- backend/db/models/__init__.py
+++ backend/db/models/__init__.py
@@ -1,4 +1,5 @@
 #  NOTE: We need to import all models in order for them to be visible to alembic
-from backend.db.models.user import User
+from backend.db.models.item import Item
+from backend.db.models.shopping_list import ShoppingList
 
-__all__ = ["User"]
+__all__ = ["Item", "ShoppingList"]
---
+++ backend/db/models/item.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.dialects.postgresql import UUID
+
+from backend.db.db import Base
+
+
+class Item(Base):
+    __tablename__ = "item"
+
+    id = Column(Integer, primary_key=True)
+    uid = Column(UUID(as_uuid=True), nullable=False, unique=True, index=True)
+    name = Column(String, nullable=False)
+    is_checked = Column(Boolean, default=False)
+    shopping_list = Column(Integer, ForeignKey("shopping_list.id"))
