0daaf98895fb1502d417b746ab82bc163acf3e99
mmitjam <mp.mmotja@gmail.com>
2022-03-19 19:14:16 +0200

    Add user table

--- backend/db/models/__init__.py
+++ backend/db/models/__init__.py
@@ -1,5 +1,6 @@
 #  NOTE: We need to import all models in order for them to be visible to alembic
 from backend.db.models.item import Item
 from backend.db.models.shopping_list import ShoppingList
+from backend.db.models.user import User
 
-__all__ = ["Item", "ShoppingList"]
+__all__ = ["Item", "ShoppingList", "User"]
