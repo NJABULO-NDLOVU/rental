a79dfed0a7b33e04ef090de45b890dadc1d1011a
mmitjam <mp.mmotja@gmail.com>
2022-07-16 08:52:56 +0200

    Dashboard, property view & tenant view components.

--- Angular/projects/service-portal/src/app/app.module.ts
+++ Angular/projects/service-portal/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PropertiesComponent } from './properties/properties.component';
+import { TenantsComponent } from './tenants/tenants.component';
+import { PropertyListComponent } from './properties/components/property-list/property-list.component';
+import { PropertyViewComponent } from './properties/components/property-view/property-view.component';
+import { TenantListComponent } from './tenants/components/tenant-list/tenant-list.component';
+import { TenantViewComponent } from './tenants/components/tenant-view/tenant-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    PropertiesComponent,
+    TenantsComponent,
+    PropertyListComponent,
+    PropertyViewComponent,
+    TenantListComponent,
+    TenantViewComponent
   ],
   imports: [
     BrowserModule,
