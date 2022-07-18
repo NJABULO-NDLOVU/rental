0c6566130e9f391750b4aab22e0f7e1a8a624dbe
mmitjam <mp.mmotja@gmail.com>
2022-07-18 11:35:34 +0200

    Added shared components and modules

---
+++ Angular/projects/core/src/lib/modules/font-awesome.module.ts
@@ -0,0 +1,26 @@
+import {NgModule} from '@angular/core';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCheck, faTimesCircle, faBars, faUserTimes, faUserCheck, faUserCircle, faMask} from '@fortawesome/free-solid-svg-icons';
+
+
+@NgModule({
+  imports: [
+    FontAwesomeModule,
+  ],
+  exports: [
+    FontAwesomeModule,
+  ],
+})
+export class FontAModule {
+    constructor(library: FaIconLibrary) {
+        library.addIcons(
+          faCheck,
+          faTimesCircle,
+          faBars,
+          faUserTimes,
+          faUserCheck,
+          faUserCircle,
+          faMask
+        );
+      }
+ }
